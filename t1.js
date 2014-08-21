1589b89625b9a7c5f5a1f98793d75d5505007d79
Ashley Chang <ashley.chang@digbil.com>
2014-08-21 14:19:43 +0800

    Ashley t1

---
+++ t1.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/ping', function(req, res){
+  res.send('Ashley Test');
+});
+var server = app.listen(8000, function() {
+    console.log('Ashley Test branch 1');
+});
