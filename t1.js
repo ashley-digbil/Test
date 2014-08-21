b96f7567239fae66e60464aa8291829e1752d3b6
Ashley Chang <ashley.chang@digbil.com>
2014-08-21 14:44:46 +0800

    Ashley commit 2 files

--- t1.js
+++ t1.js
@@ -1,8 +1,9 @@
+var a="";
 var express = require('express');
 var app = express();
 app.get('/ping', function(req, res){
   res.send('Ashley Test');
 });
 var server = app.listen(8000, function() {
-    console.log('Ashley Test branch 1');
+    console.log('Ashley Test branch 2');
 });
