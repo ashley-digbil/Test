ef72fc1d79a233c928ff67e801beef5d6e67f761
Ashley Chang <ashley.chang@digbil.com>
2014-08-21 15:04:42 +0800

    Ashley create new branch

--- t1.js
+++ t1.js
@@ -4,5 +4,5 @@ app.get('/ping', function(req, res){
   res.send('Ashley Test');
 });
 var server = app.listen(8000, function() {
-    console.log('Ashley Test branch 1');
+    console.log('Ashley Test branch 2');
 });
